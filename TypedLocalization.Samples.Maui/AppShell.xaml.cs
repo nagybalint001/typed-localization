3f46199f289bbaee7ca5463c0393ca395e2d837d
Tóth Tibor <toth.tibor@autsoft.hu>
2022-08-29 14:30:26 +0200

    repro

---
+++ TypedLocalization.Samples.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TypedLocalization.Samples.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TypedLocalization.Samples.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TypedLocalization.Samples.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
